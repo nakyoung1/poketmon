98405688fa72c88c3c33e6a7e326c3fc91947ee2
nakyoung1 <skrud5730@naver.com>
2025-04-24 16:44:41 +0900

    env 위치랑 이미지 수정

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,15 +1,10 @@
-// Import the functions you need from the SDKs you need
+// firebase-config.js
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
      apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
      authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -20,14 +15,12 @@ const firebaseConfig = {
      measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
+// ✅ 앱 한 번만 초기화
 const app = initializeApp(firebaseConfig);
+
+// ✅ 기능별 서비스 가져오기
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-const firebaseApp = initializeApp(firebaseConfig);
-const db = getFirestore(firebaseApp);
-
-export { db };
-
-export { app, auth };
+export { app, analytics, auth, db };
